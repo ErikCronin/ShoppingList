1c8c34489d556004be768d4d04966699324be135
ErikCronin <erik.cronin@my.jcu.edu.au>
2016-09-08 18:58:54 +1000

    updated code
    added incomplete "A" function and descriptions for functions

--- ErikCroninA1.py
+++ ErikCroninA1.py
@@ -15,9 +15,16 @@ def main():
 
         # Add New Items
         elif menu_choice == "A":
-            print("You chose A")
-        # Mark an item as completed
+            new_item_list = ""
+            item_name = str(input("Enter the name of the new item: "))
+            item_price = str(input("Enter the price of the new item: "))
+            item_priority = str(input("Enter the priority of the new item: "))
+            new_item_list = new_item_list.join(item_name + item_price + item_priority)
+            print(new_item_list)
+            list_of_items = list_of_items([new_item_list])
+            print(list_of_items)
 
+        # Mark an item as completed
         elif menu_choice == "M":
             ask_question = True
             while ask_question:
@@ -28,7 +35,6 @@ def main():
                         break
                     except ValueError:
                         print("You did not enter a number. Please enter a number.")
-
                 write_list = list_of_writable_items(list_of_items, "r")
                 completed_list = list_of_writable_items(list_of_items, "c")
                 if mark_completion < len(write_list):
@@ -37,7 +43,6 @@ def main():
                     completed_list.append(write_list[mark_completion])
                     write_list.remove(write_list[mark_completion])
                     list_of_items = write_list + completed_list
-                    print(list_of_items)
                     ask_question = False
                 else:
                     print("Invalid input!")
@@ -49,6 +54,7 @@ def main():
     print("exiting shopping list")
 
 
+# Function for choosing menu option
 def menu_choice_func():
     menu_select = input("Menu:\nR - List Required Items\n"
                         "C - List Completed Items\nA - Add New Item\n"
@@ -58,6 +64,7 @@ def menu_choice_func():
     return menu_select
 
 
+# Function to show required items (r) or completed items (c)
 def required_items(shopping_list, status):
     line_number = 0
     for line_str in shopping_list:
@@ -68,6 +75,7 @@ def required_items(shopping_list, status):
             line_number += 1
 
 
+# Function to create a formattable list of items in the shopping list
 def marked_func(shopping_list):
     list_of_items = []
     for item in shopping_list:
@@ -77,6 +85,7 @@ def marked_func(shopping_list):
     return list_of_items
 
 
+# Function to add items to completed list
 def list_of_writable_items(shopping_list, status):
     write_list = []
     for line_str in shopping_list:
